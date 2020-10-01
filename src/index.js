06bac6fdd535b3e97726e9794d257a0cee67b0d7
gooanq <ivanguzikxv@gmail.com>
2020-10-02 01:25:02 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+
+  if(!Array.isArray(array) || array.length===0 || arguments.length === 0){
+    return 0;
+  }
+
+  return array.reduce((item, min) => {
+    if (item < min) {
+      return item;
+    }
+    return min
+  }, array[0]);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+
+  if(!Array.isArray(array) || array.length===0 || arguments.length === 0){
+    return 0;
+  }
+
+  return array.reduce((item, max) => {
+    if (item > max) {
+      return item;
+    }
+    return max
+  }, array[0]);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+
+  if(!Array.isArray(array) || array.length===0 || arguments.length === 0){
+    return 0;
+  }
+
+  return array.reduce((item, sum) => sum += item, 0) / array.length;
 }
